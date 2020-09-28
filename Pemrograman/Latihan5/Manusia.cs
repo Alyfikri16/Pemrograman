e8c53f38393163d7bd630e1c845b889f8246fde8
Aly Dzulfikri <71118273+Alyfikri16@users.noreply.github.com>
2020-09-28 11:11:34 +0700

    Add files via upload

---
+++ Pemrograman/Latihan5/Manusia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manusia : MonoBehaviour
+{
+	public void makan(){
+		Debug.Log("Perlu Makan");}
+	public void tidur(){
+		Debug.Log("Perlu Tidur");}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
