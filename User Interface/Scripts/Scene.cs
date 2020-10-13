4005191c6eadc1435254622137eb1309f0564838
Aly Dzulfikri <71118273+Alyfikri16@users.noreply.github.com>
2020-10-13 23:01:29 +0700

    User Interface

---
+++ User Interface/Scripts/Scene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	public void MenuScene(){
+		SceneManager.LoadScene ("Menu");
+	}
+		
+	public void LoadingScene(){
+		SceneManager.LoadScene ("Loading");
+	}
+		
+	public void KeluarScene(){
+		Application.Quit ();
+	}
+	// Use this for initialization
+	void Start (){ 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+}
+}
