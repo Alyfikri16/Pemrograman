5aca1b9fcef767da9490a864f8f57eb550393f11
Aly Dzulfikri <71118273+Alyfikri16@users.noreply.github.com>
2020-10-13 22:58:27 +0700

    PemrogramanGame

---
+++ Latihan2/Latihan2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan2 : MonoBehaviour
+{
+	public int nilaiA;
+	public int nilaiB;
+	[Range(1,5)]
+	public int nilaiC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int total = (nilaiA + nilaiB)*nilaiC;
+		Debug.Log("Total "+total);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Latihan5/Prajurit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prajurit : Manusia
+{
+	public void serang(){
+		Debug.Log("Menyerang");}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("Seorang Perajurit dapat : ");
+		serang();
+		makan();
+		tidur();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
