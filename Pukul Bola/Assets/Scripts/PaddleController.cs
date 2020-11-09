32a804ff421f014dc960661082cbd29f6b396221
Aly Dzulfikri <71118273+Alyfikri16@users.noreply.github.com>
2020-11-09 13:09:00 +0700

    Pukul Bola

---
+++ Pukul Bola/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+	public float kecepatan;
+	public string axis;
+	public float BatasAtas;
+	public float BatasBawah;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float gerak = Input.GetAxis (axis) * kecepatan * Time.deltaTime;
+		float nextPos = transform.position.y+gerak;
+		if (nextPos > BatasAtas){
+			gerak = 0;
+		}
+		if (nextPos < BatasBawah){
+			gerak = 0;
+		}
+		transform.Translate (gerak, 0, 0);
+	}
+}
